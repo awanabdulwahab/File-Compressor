974b65d7743f85316c45231ef3125fa6968f55c4
abubakar1999 <58000340+abubakar1999@users.noreply.github.com>
2019-12-08 23:05:55 +0500

    Add files via upload

---
+++ File Compressor/AOA_Project/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace AOA_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FileDialogue = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_Broswer = new System.Windows.Forms.Button();
+            this.btn_Compress = new System.Windows.Forms.Button();
+            this.btn_Decompress = new System.Windows.Forms.Button();
+            this.Welcome = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // FileDialogue
+            // 
+            this.FileDialogue.Location = new System.Drawing.Point(204, 120);
+            this.FileDialogue.Multiline = true;
+            this.FileDialogue.Name = "FileDialogue";
+            this.FileDialogue.Size = new System.Drawing.Size(374, 30);
+            this.FileDialogue.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(55, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 19);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Select a File";
+            // 
+            // btn_Broswer
+            // 
+            this.btn_Broswer.Font = new System.Drawing.Font("Times New Roman", 12.25F, System.Drawing.FontStyle.Bold);
+            this.btn_Broswer.Location = new System.Drawing.Point(619, 117);
+            this.btn_Broswer.Name = "btn_Broswer";
+            this.btn_Broswer.Size = new System.Drawing.Size(96, 33);
+            this.btn_Broswer.TabIndex = 6;
+            this.btn_Broswer.Text = "Broswer";
+            this.btn_Broswer.UseVisualStyleBackColor = true;
+            this.btn_Broswer.Click += new System.EventHandler(this.btn_Broswer_Click);
+            // 
+            // btn_Compress
+            // 
+            this.btn_Compress.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Compress.Location = new System.Drawing.Point(204, 184);
+            this.btn_Compress.Name = "btn_Compress";
+            this.btn_Compress.Size = new System.Drawing.Size(95, 32);
+            this.btn_Compress.TabIndex = 10;
+            this.btn_Compress.Text = "Compress";
+            this.btn_Compress.UseVisualStyleBackColor = true;
+            this.btn_Compress.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_Decompress
+            // 
+            this.btn_Decompress.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Decompress.Location = new System.Drawing.Point(471, 184);
+            this.btn_Decompress.Name = "btn_Decompress";
+            this.btn_Decompress.Size = new System.Drawing.Size(107, 32);
+            this.btn_Decompress.TabIndex = 11;
+            this.btn_Decompress.Text = "Decompress";
+            this.btn_Decompress.UseVisualStyleBackColor = true;
+            this.btn_Decompress.Click += new System.EventHandler(this.btn_Decompress_Click);
+            // 
+            // Welcome
+            // 
+            this.Welcome.AutoSize = true;
+            this.Welcome.Font = new System.Drawing.Font("Times New Roman", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Welcome.Location = new System.Drawing.Point(310, 24);
+            this.Welcome.Name = "Welcome";
+            this.Welcome.Size = new System.Drawing.Size(158, 42);
+            this.Welcome.TabIndex = 13;
+            this.Welcome.Text = "Welcome";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(747, 450);
+            this.Controls.Add(this.Welcome);
+            this.Controls.Add(this.btn_Decompress);
+            this.Controls.Add(this.btn_Compress);
+            this.Controls.Add(this.FileDialogue);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_Broswer);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox FileDialogue;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_Broswer;
+        private System.Windows.Forms.Button btn_Compress;
+        private System.Windows.Forms.Button btn_Decompress;
+        private System.Windows.Forms.Label Welcome;
+    }
+}
+
---
+++ File Compressor/AOA_Project/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+namespace AOA_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Broswer_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string fileName = openFileDialog1.FileName;
+                FileDialogue.Text = fileName;
+                MessageBox.Show("File Uploaded Successfully");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //txt_To_Compress.MaxLength = 2000000000;
+            //string Example = txt_To_Compress.Text;
+            //var huffman = new Huffman<char>(Example);
+            //List<int> encoding = huffman.Encode(Example);
+            //List<char> decoding = huffman.Decode(encoding);
+            //var outstring = new string(decoding.ToArray());
+            //MessageBox.Show("Encoding Worked ");
+            //TextWriter txt = new StreamWriter("C:\\Temp\\demo.txt");
+            //txt.Write(Example);
+            //txt.Close();
+            string path = FileDialogue.Text;
+            FileStream sourceFile = File.OpenRead(path);
+            FileStream destinationFile = File.Create(path + ".zip");
+
+            byte[] buffer = new byte[sourceFile.Length];
+            sourceFile.Read(buffer, 0, buffer.Length);
+
+            using (GZipStream output = new GZipStream(destinationFile,
+                CompressionMode.Compress))
+            {
+                Console.WriteLine("Compressing {0} to {1}.", sourceFile.Name,
+                    destinationFile.Name, false);
+
+                output.Write(buffer, 0, buffer.Length);
+            }
+
+            // Close the files.
+            sourceFile.Close();
+            destinationFile.Close();
+            MessageBox.Show("File Compressed Sucessfully");
+        }
+        public static void Decompress(FileInfo fileToDecompress)
+        {
+            using (FileStream originalFileStream = fileToDecompress.OpenRead())
+            {
+                string currentFileName = fileToDecompress.FullName;
+                string newFileName = currentFileName.Remove(currentFileName.Length - fileToDecompress.Extension.Length);
+
+                using (FileStream decompressedFileStream = File.Create(newFileName))
+                {
+                    using (GZipStream decompressionStream = new GZipStream(originalFileStream, CompressionMode.Decompress))
+                    {
+                        decompressionStream.CopyTo(decompressedFileStream);
+                        Console.WriteLine("Decompressed: {0}", fileToDecompress.Name);
+                    }
+                }
+                MessageBox.Show("File Decompressed Successfully");
+            }
+        }
+            private void btn_Decompress_Click(object sender, EventArgs e)
+            {
+            string path = FileDialogue.Text;
+            FileInfo filepath = new FileInfo(path);
+            Decompress(filepath);
+            }
+            //    string startPath = @".\start";
+            //    string zipPath = @".\result.zip";
+            //    string extractPath = @".\extract";
+
+            //    ZipFile.CreateFromDirectory(startPath, zipPath);
+
+            //    ZipFile.ExtractToDirectory(zipPath, extractPath)
+    }
+}
